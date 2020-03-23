248cf7a78a94147c422d266fe4df1dd15b3d8913
pluxj <651107734@qq.com>
2020-03-23 20:28:28 +0800

    reverse-integer

--- leetcode/7.reverse-integer.cpp
+++ leetcode/7.reverse-integer.cpp
@@ -14,6 +14,10 @@ public:
         int ret = 0;
         while(x / 10)
         {
+            if(ret > INT_MAX/10 || (ret == INT_MAX / 10 && x/10 > 7))
+                return 0;
+            if(ret < INT_MIN / 10 || (ret == INT_MIN / 10 && x / 10 < -8))
+                return 0;
             ret = ret * 10 + x%10;
             x = x / 10;
         }
